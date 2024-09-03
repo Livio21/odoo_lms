915dfec4a4b921250a5361072b69d4022ef4010b
livio.macaj <livio.macaj@cit.edu.al>
2024-09-03 19:09:46 +0300

    On branch master
    Initial commit
    
    Changes to be committed:
    	new file:   __init__.py
    	new file:   __manifest__.py
    	new file:   models/__init__.py
    	new file:   models/models.py
    	new file:   security/ir.model.access.csv

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Learning Management System',
+    'version': '1.0',
+    'summary': 'LMS made on ODOO',
+    'description': 'LMS made on ODOO',
+    'category': 'Test',
+    'author': 'Livio Macaj',
+    'website': 'Website',
+    'depends': ['base'],
+    'data': [
+        'views/',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True
+}
---
+++ models/models.py
@@ -0,0 +1,5 @@
+from odoo import models, fields, api
+
+
+class TestModule(models.Model):
+    _name = 'odoo_lms.TestModule'
